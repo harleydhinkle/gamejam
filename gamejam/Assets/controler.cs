2125dc0b02b911eb5736b0cc6e543a71a82a2117
Unknown <wolfking068@gmail.com>
2018-11-06 08:29:08 -0800

    yo

---
+++ gamejam/Assets/controler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controler : MonoBehaviour {
+    public float horizontal;
+    public float vertical;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            vertical = 1;
+        }
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+        {
+            vertical = -1;
+        }
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            horizontal = -1;
+        }
+        if (Input.GetKey(KeyCode.D) && !Input.GetKeyDown(KeyCode.A))
+        {
+            horizontal = 1;
+        }
+        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            horizontal = 0;
+        }
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            vertical = 0;
+        }
+    }
+}
---
+++ gamejam/Assets/movemint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movemint : MonoBehaviour {
+    public float speed;
+    public controler controller;
+    Vector3 desierv;
+    public float drag;
+    float horizontal;
+    float vertical;
+    public Rigidbody rm;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal = controller.horizontal;
+        Vector3 myright = new Vector3(horizontal, 0, 0);
+        vertical = controller.vertical;
+        Vector3 myup = new Vector3(0, 0, vertical);
+        desierv = (myup + myright) * speed * Time.deltaTime;
+        rm.velocity = desierv;
+    }
+}
